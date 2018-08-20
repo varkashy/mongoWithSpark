b428cb1324554d16eedd2534c52cfa8ff7f40458
varkashy <varunkkashyap@gmail.com>
2018-08-20 18:09:23 -0500

    user session changes

--- create_blog_authors/src/main/java/course/UserDAO.java
+++ create_blog_authors/src/main/java/course/UserDAO.java
@@ -21,6 +21,7 @@ import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import sun.misc.BASE64Encoder;
 
 import java.io.UnsupportedEncodingException;
@@ -48,15 +49,19 @@ public class UserDAO {
         // create an object suitable for insertion into the user collection
         // be sure to add username and hashed password to the document. problem instructions
         // will tell you the schema that the documents must follow.
+        Document userdoc=new Document("_id",username).append("password",passwordHash);
+
 
         if (email != null && !email.equals("")) {
             // XXX WORK HERE
             // if there is an email address specified, add it to the document too.
+            userdoc.append("email",email);
         }
 
         try {
             // XXX WORK HERE
             // insert the document into the user collection here
+            usersCollection.insertOne(userdoc);
             return true;
         } catch (MongoWriteException e) {
             if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
@@ -72,6 +77,8 @@ public class UserDAO {
 
         // XXX look in the user collection for a user that has this username
         // assign the result to the user variable.
+        Document filterDoc=new Document("_id",username);
+        user=usersCollection.find().filter(filterDoc).first();
 
         if (user == null) {
             System.out.println("User not in database");
